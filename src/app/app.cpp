f97f80b9e1af9ed70e7c86ca52e8141d09be02f0
EstevanGAY <estevan.gay@gmail.com>
2022-01-28 09:43:24 +0100

    move includes of app.cpp to app.h

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -1,12 +1,4 @@
 #include "app.h"
-#include "app/queries/sql_query.h"
-
-#include "app/enumerations/query_type.h"
-#include "app/queries/query_generator.h"
-#include "app/Exceptions/custom_exception.h"
-
-#include <memory>
-#include <algorithm>
 
 void app::start(int argc, char const *argv[]) {
 
@@ -57,24 +49,6 @@ void app::start(int argc, char const *argv[]) {
             sql.erase(0, pos + delimiter.length());
         }
         std::cout << sql << std::endl;*/
-
-        /*try {
-            unique_ptr<sql_query> query = query_factory::generate_query(sql);
-            query->check();
-            query->expand();
-            query->execute();
-        } catch (unknown_sql_exception &e) {
-
-        } catch (sql_exception &e) {
-
-        } catch (check_exception &e) {
-
-        } catch (expand_exception &e) {
-
-        } catch (execute_exception &e) {
-
-        } catch (exception & e) {
-        }*/
 
 
     } while (true);
--- src/app/app.h
+++ src/app/app.h
@@ -1,8 +1,18 @@
 #ifndef ESTESQL_APP_H
+#define ESTESQL_APP_H
 
 #include <iostream>
+#include <memory>
+#include <algorithm>
 
-#define ESTESQL_APP_H
+#include "app/queries/sql_query.h"
+#include "app/queries/query_generator.h"
+
+#include "app/enumerations/query_type.h"
+
+#include "app/exceptions/custom_exception.h"
+
+
 
 using namespace std;
 
