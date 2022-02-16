8370ea93c51d541f6aa89e49095021475c84aacb
Loïc BERTRAND <loic.bert.marce@gmail.com>
2022-02-16 17:00:08 +0100

    add update key

--- src/app/managers/key_file.cpp
+++ src/app/managers/key_file.cpp
@@ -11,9 +11,30 @@ key_file *key_file::get_instance() {
 }
 
 uint64_t key_file::get_next_key() {
-    return 0;
+    if(!exists()) {
+        update_key(0);
+        return 1;
+    }
+
+    open();
+
+    file.seekg(0);
+
+    char key_data[UNSIGNED_LONG_LONG_SIZE_IN_BITS];
+    file.read(key_data, UNSIGNED_LONG_LONG_SIZE_IN_BITS);
+    string key_binary_string = (string) key_data;
+
+    close();
+
+    return stoi(key_binary_string, nullptr, 2);
 }
 
 void key_file::update_key(uint64_t last_value) {
+    open();
 
+    file.seekp(0);
+
+    file << bitset<UNSIGNED_LONG_LONG_SIZE_IN_BITS>(last_value);
+
+    close();
 }
